d705a0df79d45c99dac25d54edfa3ecb451f78cf
Abhinav Reddy Pannala <36763371+abhinavpannala@users.noreply.github.com>
2023-11-09 14:44:22 -0600

    Time: 124 ms (59.8%), Space: 19.3 MB (8.13%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums1 = list()
+        nums1 = set()
         for i in range(len(nums)):
             if nums[i] in nums1:
                 nums1.remove(nums[i])
             else:
-                nums1.append(nums[i])
-        return nums1[0]
+                nums1.add(nums[i])
+        return nums1.pop()
             
             
