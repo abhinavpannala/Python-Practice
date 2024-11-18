b6df73001f872f6681fc3ce1636f9c9151621661
Abhinav Reddy Pannala <36763371+abhinavpannala@users.noreply.github.com>
2024-11-18 07:19:32 -0600

    Time: 3 ms (47.06%), Space: 11.6 MB (74.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -9,13 +9,10 @@ class Solution(object):
         hash['(']=')'
         hash['{']='}'
         hash['[']=']'
-        last_opened = []
         for item in s:
-            if last_opened and item == hash.get(last_opened[-1]):
+            if stack and item == hash.get(stack[-1]):
                 stack.pop()
-                last_opened.pop()
             else:
-                last_opened.append(item)
                 stack.append(item)
         if stack:
             return False
