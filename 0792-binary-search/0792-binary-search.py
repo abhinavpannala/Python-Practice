366d710f71094e317e22a539fcf039d348e0b4a5
Abhinav Reddy Pannala <36763371+abhinavpannala@users.noreply.github.com>
2024-11-28 14:06:45 -0600

    Time: 0 ms (100%), Space: 13.2 MB (11.21%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -5,14 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        def binary_search(left,right):
-            if left>right:
-                return -1
+        # def binary_search(left,right):
+        #     if left>right:
+        #         return -1
+        #     mid = (left+right)//2
+        #     if target == nums[mid]:
+        #         return mid
+        #     elif target < nums[mid]:
+        #         return binary_search(left,mid-1)
+        #     else:
+        #         return binary_search(mid+1,right)
+        # return binary_search(0, len(nums)-1)
+        left, right = 0, len(nums)-1
+        while(left<=right):
             mid = (left+right)//2
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
-            elif target < nums[mid]:
-                return binary_search(left,mid-1)
-            else:
-                return binary_search(mid+1,right)
-        return binary_search(0, len(nums)-1)
+            elif nums[mid]>target:
+                right = mid-1
+            elif nums[mid]<target:
+                left = mid+1
+        return -1
