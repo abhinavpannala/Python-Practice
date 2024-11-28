c812c4292294368d34a76be6ecbec3e13025a639
Abhinav Reddy Pannala <36763371+abhinavpannala@users.noreply.github.com>
2024-11-28 13:19:39 -0600

    Time: 9 ms (94.61%), Space: 12 MB (59.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,9 +1,22 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        tracker = collections.defaultdict(int)
-        for x in s: tracker[x] += 1
-        for x in t: tracker[x] -= 1
-        for x in tracker.values():
-            if x!=0:
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dictionary = {}
+        for i in s:
+            if i in dictionary:
+                dictionary[i]+=1
+            else:
+                dictionary[i]=1
+        for i in t:
+            if i in dictionary:
+                dictionary[i]-=1
+            else:
+                return False
+        for k in dictionary.values():
+            if k!=0:
                 return False
         return True
