3ed20daca39218017d83fd08a778f569c6a0f054
Abhinav Reddy Pannala <36763371+abhinavpannala@users.noreply.github.com>
2024-12-03 21:26:10 -0600

    Time: 0 ms (100%), Space: 17.3 MB (7.99%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -3,7 +3,7 @@ class Solution:
         i, j=len(a)-1, len(b)-1
         carry = 0
         ans=[]
-        while i>=0 or j>=0 or carry:
+        while i>=0 or j>=0:
             temp = carry
             if i>=0:
                 temp+=int(a[i])
@@ -11,6 +11,8 @@ class Solution:
             if j>=0:
                 temp+=int(b[j])
                 j-=1
-            ans += str(temp % 2) 
+            ans.append(str(temp % 2)) 
             carry = temp // 2
+        if carry:
+            ans.append("1")
         return "".join(ans[::-1])
